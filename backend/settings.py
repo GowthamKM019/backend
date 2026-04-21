66da05003cc7d7d6a501c5c7faa1b44fa2a32e7b
GowthamKM019 <gowthamkmgowda802@gmail.com>
2026-04-21 14:21:42 +0530

    add vercel url to cors

--- backend/settings.py
+++ backend/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # for local testing
-    # We'll add Vercel URL after deployment
+    "http://localhost:3000",
+    "https://code-cobra.vercel.app",  # ← your Vercel URL
 ]
